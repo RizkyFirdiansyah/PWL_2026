87e33624ebc5c8bcc4493bd79f0862548a99bb92
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-02-26 21:04:40 +0700

    P2_Routing Parameter

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,16 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return '244107020013 - Muhammad Rizky Firdiansyah';
 });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Artikel ID: ' . $id;
+});
+
