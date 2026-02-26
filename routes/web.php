d0e4d775fea274a2c98442b3b257caded49319e1
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-02-26 13:49:54 +0700

    P2_Routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat datang';
+});
+
+Route::get('/about', function () {
+    return '244107020013 - Muhammad Rizky Firdiansyah';
+});
